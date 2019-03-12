c57cd3a083876cb21d5280725a04c62952d7c0c2
melimore86 <melimore86@ufl.edu>
2019-03-12 12:09:23 -0400

    resolved issued with na.rm=TRUE, in quant(), every quant() needs to have an na.rm=TRUE, to funciton, somehow the 0s or NAs don't allow the quant to function correctly, issue resolved

--- display_quantile_app/app.R
+++ display_quantile_app/app.R
@@ -7,6 +7,7 @@ library("shiny")
 station = '02323500'   
 stinfo  = siteInfo(station)
 
+
 # Load data and dynamically check if data needs to be updated
 dis <- read_rds("data/dis1.rds")
 if (max(dis$dates) < (Sys.Date() - 5)) {
@@ -19,19 +20,19 @@ if (max(dis$dates) < (Sys.Date() - 5)) {
 
 # Data carpentries and create quantile data table
 dis_noleap <- dis %>%
-  filter(!(month(dates) == 2 & day(dates) == 29)) %>% 
-  na.omit()#<- rempving the leap day year for all years that have it
+  filter(!(month(dates) == 2 & day(dates) == 29))
+  #na.omit()#<- rempving the leap day year for all years that have it
 
 dis_quant <- dis_noleap %>%
   mutate(md = strftime(dates, format = "%m-%d")) %>%
   group_by(md) %>%
-  summarise(quan0 = quantile(val, 0),
-            quan25 = quantile(val, 0.25),
-            quan50 = quantile(val, 0.50),
-            quan75 = quantile(val, 0.75),
-            quan100 = quantile(val, 1)) %>%
-  gather("quantile", "val", -md) %>% 
-  na.omit()
+  summarise(quan0 = quantile(val, 0, na.rm = TRUE),
+            quan25 = quantile(val, 0.25, na.rm = TRUE),
+            quan50 = quantile(val, 0.50, na.rm = TRUE),
+            quan75 = quantile(val, 0.75, na.rm = TRUE),
+            quan100 = quantile(val, 1, na.rm = TRUE)) %>%
+  gather("quantile", "val", -md)
+
 dis_quant$quantile <- str_remove(dis_quant$quantile, "quan") %>%
   factor(levels = c("100", "75", "50", "25", "0"))
 
