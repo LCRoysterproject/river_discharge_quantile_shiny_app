c92913d927f53020a0f7c7e620fd55a52559d782
melimore86 <melimore86@ufl.edu>
2020-06-04 10:16:50 -0400

    looks like something was up with the min values from JM codes, this time, might be a R studio update error is lattice extra but it's not working, removed the minValues will investigate

--- river_discharge_quantile_shiny_app/app.R
+++ river_discharge_quantile_shiny_app/app.R
@@ -21,7 +21,7 @@ if (max(dis$dates) < (Sys.Date() - 5)) {
 # Data carpentries and create quantile data table
 dis_noleap <- dis %>%
   filter(!(month(dates) == 2 & day(dates) == 29))
-  #na.omit()#<- rempving the leap day year for all years that have it
+#na.omit()#<- rempving the leap day year for all years that have it
 
 dis_quant <- dis_noleap %>%
   mutate(md = strftime(dates, format = "%m-%d")) %>%
@@ -36,66 +36,57 @@ dis_quant <- dis_noleap %>%
 dis_quant$quantile <- str_remove(dis_quant$quantile, "quan") %>%
   factor(levels = c("100", "75", "50", "25", "0"))
 
-minValues <- dis_quant$val[dis_quant$quantile == '0']
-
-dis_quant$min <- minValues
-
-dis_quant1 <- subset(dis_quant, dis_quant$quantile != '0')
-
 #### UI ####
 ui <- fluidPage(
-   
-   titlePanel("Suwannee River Discharge Quantiles"),
-   
-   sidebarLayout(
-      sidebarPanel(
-        
-        h4("Percentile Description"),
-        helpText("A percentile is a value on a scale of one hundred that indicates the percent of a distribution that is equal to or below it. For example, on the map of daily streamflow conditions a river discharge at the 90th percentile is equal to or greater than 90 percent of the discharge values recorded on this day of the year during all years that measurements have been made.  In general,a percentile greater than 75 is considered above normal, a percentile between 25 and 75 is considered normal, and a percentile less than 25 is considered below normal"),
-        
-         sliderInput("yoi",
-                     "Year:",
-                     min = 1950, sep = "",
-                     max = year(Sys.Date()),
-                     value = year(Sys.Date()),
-                     step = 1)
-      ),
+  
+  titlePanel("Suwannee River Discharge Quantiles"),
+  
+  sidebarLayout(
+    sidebarPanel(
       
-      mainPanel(
-          width = 7,
-         plotOutput("quantPlot", height = "600px")
-      )
-   )
+      h4("Percentile Description"),
+      helpText("A percentile is a value on a scale of one hundred that indicates the percent of a distribution that is equal to or below it. For example, on the map of daily streamflow conditions a river discharge at the 90th percentile is equal to or greater than 90 percent of the discharge values recorded on this day of the year during all years that measurements have been made.  In general,a percentile greater than 75 is considered above normal, a percentile between 25 and 75 is considered normal, and a percentile less than 25 is considered below normal"),
+      
+      sliderInput("yoi",
+                  "Year:",
+                  min = 1950, sep = "",
+                  max = year(Sys.Date()),
+                  value = year(Sys.Date()),
+                  step = 1)
+    ),
+    
+    mainPanel(
+      width = 7,
+      plotOutput("quantPlot", height = "600px")
+    )
+  )
 )
 
 #### SERVER ####
 server <- function(input, output) {
-   
-   output$quantPlot <- renderPlot({
-     dis_quant1 <- dis_quant %>%
-       mutate(dates = paste(input$yoi, md, sep="-") %>% as.Date)
-     
-     dis_yoi <- dis_noleap %>%
-       filter(year(dates) == input$yoi)
-     
-     
-     cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2")
-     
-     
-     ggplot(dis_yoi, aes(x=dates, y=val)) +
-       ggtitle(input$yoi) +
-       ylab("River Discharge (ft^3)")+
-       xlab ("Date") +
-       guides(fill=guide_legend(title="Quantiles")) +
-       geom_ribbon(data = dis_quant1, aes(x=dates, ymax=val, ymin=min, fill=quantile)) +
-       geom_line(size=1.2) +
-       scale_fill_manual(values=cbPalette, breaks=c("100", "75", "50", "25"),
-                         labels=c("75-100", "50-75", "25-50", "0-25")) +
-       theme_minimal() +
-       theme(panel.border = element_rect(colour = "black", fill=NA, size=1))
-   })
+  
+  output$quantPlot <- renderPlot({
+    dis_quant1 <- dis_quant %>%
+      mutate(dates = paste(input$yoi, md, sep="-") %>% as.Date)
+    
+    dis_yoi <- dis_noleap %>%
+      filter(year(dates) == input$yoi)
+    
+    cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2")
+    
+    
+    ggplot(dis_yoi, aes(x=dates, y=val)) +
+      ggtitle(input$yoi) +
+      ylab("River Discharge (ft^3)")+
+      xlab ("Date") +
+      guides(fill=guide_legend(title="Quantiles")) +
+      geom_ribbon(data = dis_quant1, aes(x=dates, ymax=val, ymin=0, fill=quantile)) +
+      geom_line(size=1.2) +
+      scale_fill_manual(values=cbPalette) +
+      theme_minimal() +
+      theme(panel.border = element_rect(colour = "black", fill=NA, size=1))
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
